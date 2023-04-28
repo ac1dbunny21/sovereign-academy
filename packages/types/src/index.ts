5e6f1f9e2f06c5ff157a0f90f40e7e93da5048a0
ac1dbunny21 <github@acidbunny21.xyz>
2023-04-28 09:39:04 +0200

    feat(cross): change authentication method to JWT

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,36 +1,3 @@
-export type ChangeKind = 'added' | 'modified' | 'removed';
-
-/**
- * Changed content in a repository (agnostic to the repository)
- *
- * @example
- * ```
- * {
- *  path: 'resources/books/1/resource.yml',
- *  kind: 'added',
- *  commit: '0e4e4c599c6a171463bec46611049dd63d293f59',
- *  time: '2021-05-01T12:00:00Z',
- *  url: 'https://raw.githubusercontent.com/blc-org/sovereignacademy-data/0e4e4c599c6a171463bec46611049dd63d293f59/resources/books/1/resource.yml'
- * }
- * ```
- */
-export interface ChangedFileBase {
-  /** Path to the file */
-  path: string;
-  /** Type of the change */
-  kind: ChangeKind;
-  /** Commit hash */
-  commit: string;
-  /** Commit timestamp */
-  time: number;
-}
-
-export interface ChangedAsset extends ChangedFileBase {
-  /** URL to the file */
-  url: string;
-}
-
-export interface ChangedFile extends ChangedFileBase {
-  /** Raw data */
-  data: string;
-}
+export * from './content';
+export * from './local-storage';
+export * from './jwt';
